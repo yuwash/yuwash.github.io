c810905d66351b57d6a232f20942a5aa9923d509
Yushin Washio <dev@yuwash.eu>
2025-08-22 11:13:43 +0200

    Feat: add eleventy-plugin-toc

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,18 @@
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
+const markdownIt = require('markdown-it');
+const markdownItAnchor = require('markdown-it-anchor');
+const pluginTOC = require('eleventy-plugin-toc');
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(EleventyRenderPlugin);
   eleventyConfig.addPassthroughCopy({static: '/'});
+
+  eleventyConfig.setLibrary(
+    'md',
+    markdownIt().use(markdownItAnchor)
+  );
+  eleventyConfig.addPlugin(pluginTOC);
+
   return {
     dir: {
         input: "pages",
